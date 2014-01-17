33f3e4f1a06534715e10a8549b67e54383d9bdcb
Vinicius Mayer <viniciusmayer@gmail.com>
2014-01-17 21:13:02 -0200

    initial commit

---
+++ src/com/eleonorvinicius/androidexamples/seis/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eleonorvinicius.androidexamples.seis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.eleonorvinicius.androidexamples.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}

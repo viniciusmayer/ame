cc2fa05dd29685f5ff0fed703f08e048d8b7296c
Vinicius Mayer <viniciusmayer@gmail.com>
2014-01-20 19:32:53 -0200

    Apagando arquivos desnecessarios.

--- src/gen/com/eleonorvinicius/androidexamples/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.eleonorvinicius.androidexamples;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
